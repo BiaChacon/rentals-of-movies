cb0bd277f5f449789a6cf5038400e350f38f23ce
Bia Chacon <anabheatrizchacon@outlook.com>
2019-04-27 09:45:34 -0300

    test database

--- src/main/java/controller/TesteDatabase.java
+++ src/main/java/controller/TesteDatabase.java
@@ -1,13 +1,21 @@
 package controller;
 
+import java.sql.Connection;
 import javax.faces.bean.ManagedBean;
 import persistence.ConnectionDatabase;
 
 @ManagedBean (name = "t")
 public class TesteDatabase {
-    ConnectionDatabase cd  = new ConnectionDatabase();
-       
-            
     
+    ConnectionDatabase cd  = new ConnectionDatabase();
+    
+    public String teste(){
+        cd.dbConnection();
+        Connection c = cd.getConnection();
+        if(c == null){
+            return "conexão falhou";
+        }
+        return "Conexão deu certo";
+    }   
     
 }
